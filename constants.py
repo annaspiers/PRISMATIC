812b5ffc2d27d75ab5eb4036390047dfa76227ba
toanngosy <toanngosy@gmail.com>
2023-02-13 16:46:35 -0800

    delete unused file

--- constants.py
+++
@@ -1,6 +0,0 @@
-site = 'SOAP'
-year = 2019
-data_path = '/home/toanngo/Documents/GitHub/prisma/preprocessing/data'
-laz_path = ('/home/toanngo/Documents/GitHub/prisma/preprocessing/'
-            'data/lidar_samples')
-output_laz_path = ''
