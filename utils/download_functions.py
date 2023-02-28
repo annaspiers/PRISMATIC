fdf377db9e3c6ac365be033d9388aff852288703
toanngosy <toanngosy@gmail.com>
2023-02-28 13:03:21 -0800

    update score metric

--- utils/download_functions.py
+++ utils/download_functions.py
@@ -10,6 +10,7 @@ NEON AOP data using the NEON Data API.
 import requests
 import urllib
 import os
+import numpy as np
 
 SITES = [
     "DELA", "LENO", "TALL", "BONA", "DEJU", "HEAL",
@@ -213,7 +214,7 @@ def download_aop_files(product,
                     print(e)
 
 
-def get_overlapped_list():
+def get_overlapped_list(year_distance_threshold=2):
     dv = {}
     dl = {}
     for site in SITES:
@@ -241,25 +242,47 @@ def get_overlapped_list():
                 dl[site][y] = [m]
             else:
                 dl[site][y].append(m)
-
     ranks = {}
     for site in SITES:
         dl_site = dl[site]
         dv_site = dv[site]
         ranks[site] = {}
         for year, dv_months in dv_site.items():
-            rank = 99
-            if year in dl_site:
-                dl_months = dl_site[year]
-                min_dv_month = min(dv_months)
-                max_dv_month = max(dv_months)
-                for dl_m in dl_months:
-                    if min_dv_month <= dl_m <= max_dv_month:
-                        r = 0
-                    else:
-                        r = min(abs(dl_m-min_dv_month),
-                                abs(dl_m-max_dv_month))
-                    if r < rank:
-                        rank = r
-            ranks[site][year] = rank
-    return dv, dl
+            to_check_dl_years = [str(y) for y in range(int(year) - year_distance_threshold,
+                                                       int(year) + year_distance_threshold)]
+            scores = []
+            infos = []
+            for y in to_check_dl_years:
+                if y in dl_site:
+                    score = 99
+                    lidar_date = None
+                    dl_months = dl_site[y]
+                    min_dv_month = min(dv_months)
+                    max_dv_month = max(dv_months)
+                    for dl_m in dl_months:
+                        if min_dv_month <= dl_m <= max_dv_month:
+                            r = 0
+                            dl_m_r = dl_m
+                        else:
+                            r = min(abs(dl_m-min_dv_month),
+                                    abs(dl_m-max_dv_month))
+                            dl_m_r = dl_m
+                        if r < score:
+                            score = r
+                            lidar_date = f'{dl_m_r}-{y}'
+                    cross_year_score = abs(int(y) - int(year))/10.0
+                    score += cross_year_score
+                    scores.append(score)
+                    infos.append(lidar_date)
+            if scores:
+                min_idx = np.argmin(scores)
+                ranks[site][year] = {
+                    'score': scores[min_idx],
+                    'lidar_date': infos[min_idx],
+                    'dv_months': dv_months
+                }
+            else:
+                ranks[site][year] = {}
+    return dv, dl, ranks
+
+get_overlapped_list(2)
