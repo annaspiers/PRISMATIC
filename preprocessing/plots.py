a11c15cee6c609d3ff4e7a2e523da130f61360cb
toanngosy <toanngosy@gmail.com>
2023-03-13 11:16:16 -0700

    add process plot exception

--- preprocessing/plots.py
+++ preprocessing/plots.py
@@ -91,14 +91,32 @@ def preprocess_polygons(input_data_path,
             sum(veg_gdf.stemDiameter >= 10)
         veg_plot_metadata['total_ind_stem_lt_10'] = \
             sum(veg_gdf.stemDiameter < 10)
-        query = f'plotID == "{plot_id}"'
-        sampling_area = sampling_effort.query(query)
-        sampling_area_trees = (sampling_area
-                               .totalSampledAreaTrees
-                               .values[0])
-        sampling_area_sapling = (sampling_area
-                                 .totalSampledAreaShrubSapling
-                                 .values[0])
+
+        sampling_area_trees = group.area.values[0]
+        sampling_area_sapling = group.area.values[0]
+        try:
+            query = f'plotID == "{plot_id}"'
+            sampling_area = sampling_effort.query(query)
+            v = (sampling_area
+                 .totalSampledAreaTrees
+                 .values[0])
+            if np.isnan(v):
+                log.warning(f'Site: {site}, plot: {plot_id} '
+                            'has empty sampling_area_trees')
+            else:
+                sampling_area_trees = v
+            v = (sampling_area
+                 .totalSampledAreaShrubSapling
+                 .values[0])
+            if np.isnan(v):
+                log.warning(f'Site: {site}, plot: {plot_id} '
+                            'has empty sampling_area_sapling')
+            else:
+                sampling_area_sapling = v
+        except IndexError:
+            log.warning(f'Site: {site}, plot: {plot_id} '
+                         'does not have in: pp_sampling_effort.csv')
+
         veg_plot_metadata['sampling_effort_trees'] = sampling_area_trees
         veg_plot_metadata['sampling_effort_sapling'] = sampling_area_sapling
         sampling_side = int(np.sqrt(sampling_area_trees))
