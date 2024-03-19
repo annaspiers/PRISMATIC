5c4d4204c60aaa08818eadad60f7416ea327199c
Anna Spiers <anna.spiers@colorado.edu>
2024-03-18 17:30:53 -0700

    try to force install

--- utils/install_pkgs.R
+++ utils/install_pkgs.R
@@ -1,12 +1,15 @@
-if (!require(ggbiplot)){
+if (!require("ggbiplot")){
     install.packages("ggbiplot")
+    library(ggbiplot)
 }
 
-if (!require(neonOS)){
+if (!require("neonOS")){
     install.packages("neonOS")
+    library(neonOS)
 }
 
-if (!require(geoNEON)){
+if (!require("geoNEON")){
     install.packages("devtools")
     devtools::install_github("NEONScience/NEON-geolocation/geoNEON")
+    library(geoNEON)
 }
